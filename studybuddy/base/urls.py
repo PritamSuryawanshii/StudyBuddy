c234d50c996f1442b0366bca174757d83393b415
pritamsuryawanshii <pritamsuryawanshi34@gmail.com>
2025-01-26 12:56:56 +0530

    timestamp 5:27

--- studybuddy/base/urls.py
+++ studybuddy/base/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
      path('register/', views.registerPage, name='register'),
 
      path('delete-message/<str:pk>', views.deleteMessage, name='delete-message'),
+     path('update-user/', views.updateUser, name='update-user'),
 
      path('profile/<str:pk>', views.userProfile, name='user-profile'),
      
--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Room, Topic, Message
+from .models import Room, Topic, Message, User
 from django.db.models import Q
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -36,22 +36,20 @@ def home(request):
 
 
 
-def room(request, pk): 
-    #pk (primary key)
+def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all()
-
     participants = room.participants.all()
 
     if request.method == 'POST':
-        message = Message.objects.create( 
-            user = request.user,
-            room = room,
-            body = request.POST.get('body')
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
         )
         room.participants.add(request.user)
         return redirect('room', pk=room.id)
-    
+
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context) 
 
@@ -191,3 +189,22 @@ def userProfile(request, pk):
 
     context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context)
+
+
+
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    
+    return render(request, 'base/update_user.html', {'form': form})
+
+
